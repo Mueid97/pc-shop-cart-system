d38233932ad7110bcc1098db4aea8813881333cb
Mueid <mrmuyed579@gmail.com>
2022-02-16 00:26:00 +0600

    js second section done

--- js/script.js
+++ js/script.js
@@ -5,3 +5,20 @@ function allProducts(itemName,itemId,price){
         addItems(itemId,price)
     });
 }
+
+function addItems(itemId,price){
+    const memoryCost = document.getElementById(itemId);
+    memoryCost.innerText = price;
+ 
+    const bestPrice = document.getElementById('best-price').innerText;
+    const memoryPrice = document.getElementById('memory-cost').innerText;
+    const storagePrice = document.getElementById('storage-cost').innerText;
+    const deliveryPrice = document.getElementById('delivery-cost').innerText;
+ 
+    const total = parseFloat(bestPrice) + parseFloat(memoryPrice) + parseFloat(storagePrice) + parseFloat(deliveryPrice);
+ 
+    const totalPrice = document.getElementById('total-price');
+    totalPrice.innerText = total;
+    
+ }
+ 
